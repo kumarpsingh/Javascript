d0cdcb1c9b929acc87757e98a1f9d9733e9a859c
kumarpsinghin <kumarpsinghin@yahoo.co.uk>
2016-12-28 07:22:57 +0000

    Callback function

---
+++ Index.js
@@ -0,0 +1,6 @@
+var friends = ["India", "UK", "USA", "Japan"];
+debugger;
+	friends.forEach(function (MyName, Myindex){
+	console.log(Myindex + 1 + ". " + MyName); // 1. India, 2. UK, 3. USA, 4. Japan
+	});
+
